82707c2f2929c0f58d740417a2f9eb2410fee154
melekalan <dt.bmaslan@gmail.com>
2022-04-15 19:48:15 +0300

    iam-role-policy added

---
+++ 007.iamrole-policy/iam-role-policy.tf
@@ -0,0 +1,51 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.10.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+###################################
+# IAM ROLE FOR EC2-S3 FULL ACCESS
+###################################
+
+
+resource "aws_iam_role" "ec2-s3" {
+  name = "ec2-s3-full-tf"
+  assume_role_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Principal" : {
+          "Service" : "ec2.amazonaws.com"
+        },
+        "Action" : "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "s3-full" {
+  name = "s3-full-tf"
+  role = aws_iam_role.ec2-s3.id
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "s3:*",
+          "s3-object-lambda:*"
+        ],
+        "Resource" : "*"
+      }
+    ]
+  })
+}
